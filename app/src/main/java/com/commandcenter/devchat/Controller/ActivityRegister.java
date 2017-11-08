2abfe4577c9e1621456505cd19fcaae33dd15885
Mike <inkedgfx@tampabay.rr.com>
2017-11-08 08:47:43 -0500

    add - chatbox single message row

---
+++ app/src/main/java/com/commandcenter/devchat/Controller/ActivityRegister.java
@@ -0,0 +1,15 @@
+package com.commandcenter.devchat.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.commandcenter.devchat.R;
+
+public class ActivityRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
