36683ddf0e2343423c928b37b86ed4e884aa75a3
Mike <inkedgfx@tampabay.rr.com>
2017-11-07 23:52:30 -0500

    add - login layout / firebase auth

--- app/src/main/java/com/commandcenter/devchat/Controller/MainActivity.java
+++ app/src/main/java/com/commandcenter/devchat/Controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.commandcenter.devchat;
+package com.commandcenter.devchat.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.commandcenter.devchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
