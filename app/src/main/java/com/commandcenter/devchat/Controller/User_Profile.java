d5744f99f02b26ef7ec1b794a121bc65ca4c4d23
Mike <inkedgfx@tampabay.rr.com>
2017-11-15 08:37:11 -0500

    add - UserList Activity / User_Profile Activity

---
+++ app/src/main/java/com/commandcenter/devchat/Controller/User_Profile.java
@@ -0,0 +1,15 @@
+package com.commandcenter.devchat.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.commandcenter.devchat.R;
+
+public class User_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__profile);
+    }
+}
